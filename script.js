77db9817766b32fc3db5d1206ea9894635dadd30
DeepakChowdry-bit <deepakchowdry07@gmail.com>
2023-05-21 22:20:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let nav = document.getElementById("nav")
+let menu = document.getElementById("menu")
+let body = document.getElementsByTagName("body")
+
+function sidemenu() {
+    nav.classList.toggle('open')
+}
+
+let moon = document.getElementById("moon")
+let sun = document.getElementById("sun")
+
+
+function bgchange() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
